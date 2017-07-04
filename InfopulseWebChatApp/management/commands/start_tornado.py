049438c6f1a25f0c3f1a91dc20d2d79374fbb53e
galkin <galkin@unicyb.kiev.ua>
2017-07-04 20:21:23 +0300

    add start_tornado

--- InfopulseWebChatApp/management/commands/start_tornado.py
+++ InfopulseWebChatApp/management/commands/start_tornado.py
@@ -2,14 +2,15 @@ from tornado.options import options, define
 import django.core.handlers.wsgi
 import tornado.httpserver, tornado.ioloop
 import tornado.web, tornado.wsgi
-import WebChatApp.chat
 import sockjs.tornado
+
+from InfopulseWebChatApp.chat import SocketHandler
 
 define('port', type=int, default=8888)
 wsgi_app = tornado.wsgi.WSGIContainer(django.core.handlers.wsgi.WSGIHandler())
 
 tornado_app = tornado.web.Application(
-    sockjs.tornado.SockJSRouter(WebChatApp.chat.SocketHandler, '/sock').urls
+    sockjs.tornado.SockJSRouter(SocketHandler, '/sock').urls
     + [('.*', tornado.web.FallbackHandler,dict(fallback=wsgi_app)),])
 
 server = tornado.httpserver.HTTPServer(tornado_app)
